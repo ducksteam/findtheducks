3911b1b278f3b216991d8cf09593739523a277e4
SkySourced <58579786+SkySourced@users.noreply.github.com>
2023-06-20 08:50:46 +1200

    added database connection

---
+++ db.js
@@ -0,0 +1,3 @@
+import postgres from "postgres";
+const sql = postgres('postgres://ducksadmin:carpet!and6envisage@ducks-server.postgres.database.azure.com/ducks?sslmode=require');
+export default sql;
