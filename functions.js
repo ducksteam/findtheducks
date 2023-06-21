a51b9a855490260d064ce2dae49f7518eff8bbe7
SkySourced <58579786+SkySourced@users.noreply.github.com>
2023-06-21 17:24:30 +1200

    Added login function, sessions not saving yet

--- functions.js
+++ functions.js
@@ -47,21 +47,39 @@ async function login(email, password) {
 	// Check email is valid
 	const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 	if (!email.match(mailFormat)) {
-		return "Invalid email";
+		return {"status": "error", "msg": "Invalid email"};
 	}
 
 	// Check email has associated account
-	let emailCheck = sql`select * from users where email = ${email}`;
+	let emailCheck = await sql`select * from users where email = ${email}`;
 	if (emailCheck.length === 0) {
-		return "Email not found";
+		return {"status": "error", "msg": "Email not found"};
 	}
-	
+
 	// Check password is correct
-	const passwordCheck = bcrypt.compare(password, emailCheck[0].password_hash);
+	const passwordCheck = await bcrypt.compare(password, emailCheck[0].password_hash);
 	if (!passwordCheck) {
-		return "Incorrect password";
+		return {"status": "error", "msg": "Incorrect password"};
 	}
 	
+	let sessionId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16); // Generate random session ID
+	let sessionCheck = await sql`select * from sessions where session_id = ${sessionId}`; // Check if session ID is already in use
+	while (sessionCheck.length !== 0) {
+		sessionId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16); // Generate new session ID if already in use
+		sessionCheck = await sql`select * from sessions where session_id = ${sessionId}`;
+	}
+	console.log("found session id");
+	let expiryDate = new Date(Date.now()); // Get current date
+	expiryDate.setDate(expiryDate.getDate() + 14); // Set expiry date to 14 days from now
+	expiryDate = expiryDate.toISOString().slice(0, 19).replace("T", " "); // Convert to SQL datetime format
+	try {
+		await sql`insert into sessions (session_id, user_id, expiry) values (${sessionId}, ${emailCheck[0].id}, ${expiryDate})`; // Insert session ID into database
+	} catch (error) {
+		console.error(error);
+		return {"status": "error", "msg": "Error inserting into database"};
+	}
+	console.log("inserted session id");
+	return {"status": "success", "msg": sessionId};
 }
 
 export { register, login };
--- routes/users.js
+++ routes/users.js
@@ -24,8 +24,15 @@ router.post("/register", async (req, res) => { // Handle register form submissio
 
 router.post("/login", async (req, res) => { // Handle login form submission
 	const { email, password } = req.body;
-	const error = await login(email, password);
-	res.redirect("/users/login?error=" + encodeURIComponent(error));
+	const attempt = await login(email, password);
+	if(attempt.status === "success"){
+		console.log("Logged in");
+		req.session.loggedIn = true;
+		req.session.sessionId = attempt.msg;
+		res.redirect("profile");
+	} else if(attempt.status === "error"){
+		res.redirect("/users/login?error=" + encodeURIComponent(attempt.msg));
+	}
 });
 
 router.use(express.static("public")); // Serve static files
