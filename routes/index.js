736578c8b69924df7b04d002a78d2f0e1d76e1a2
SkySourced <58579786+SkySourced@users.noreply.github.com>
2023-07-06 20:34:53 +1200

    added code for footer template

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 const router = express.Router();
 import { entry, getScoreboard, insertDuck } from "../functions.js";
+import duckFact from "../duckFacts.js";
 
 router.get("/", (req, res) => { // Serve home page
-	res.render("index", { pageTitle: "home", authorised: req.session.authorised, permissions: req.session.permissions });
+	res.render("index", { pageTitle: "home", authorised: req.session.authorised, permissions: req.session.permissions, duckFact: duckFact()  });
 });
 
 router.get("/entry", (req, res) => { // Serve entry page
 	if(req.session.authorised){
 		const status = decodeURIComponent(req.query.status) || "";
-		res.render("entry", { pageTitle: "duck entry", authorised: req.session.authorised, permissions: req.session.permissions, status });
+		res.render("entry", { pageTitle: "duck entry", authorised: req.session.authorised, permissions: req.session.permissions, status, duckFact: duckFact() });
 	} else {
 		res.redirect("users/login?status=" + encodeURIComponent("Please log in to enter a duck"));
 	}
@@ -17,15 +18,15 @@ router.get("/entry", (req, res) => { // Serve entry page
 
 router.get("/scoreboard", async (req, res) => { // Serve scoreboard page
 	let scoreboard = await getScoreboard();
-	res.render("scoreboard", { pageTitle: "scoreboard", authorised: req.session.authorised, permissions: req.session.permissions, scoreboard });
+	res.render("scoreboard", { pageTitle: "scoreboard", authorised: req.session.authorised, permissions: req.session.permissions, scoreboard, duckFact: duckFact() });
 });
 
 router.get("/newduck", (req, res) => { // Serve new duck page
 	if(req.session.permissions > 0){
 		const status = decodeURIComponent(req.query.status) || "";
-		res.render("newduck", { pageTitle: "new duck", authorised: req.session.authorised, permissions: req.session.permissions, status });
+		res.render("newduck", { pageTitle: "new duck", authorised: req.session.authorised, permissions: req.session.permissions, status, duckFact: duckFact() });
 	} else {
-		res.status(403).render("errors/403", { pageTitle: "403", authorised: req.session.authorised, permissions: req.session.permissions });
+		res.status(403).render("errors/403", { pageTitle: "403", authorised: req.session.authorised, permissions: req.session.permissions, duckFact: duckFact() });
 	}
 });
 
@@ -34,7 +35,7 @@ router.post("/newduck", async (req, res) => { // Handle new duck form submission
 		const status = await insertDuck(req, req.body.code, req.body.location);
 		res.redirect("newduck?status=" + encodeURIComponent(status));
 	} else {
-		res.status(403).render("errors/generic", { errorCode: 403, message: "Sorry, you don't have permission to view this page.", pageTitle: "403", authorised: req.session.authorised, permissions: req.session.permissions });
+		res.status(403).render("errors/generic", { errorCode: 403, message: "Sorry, you don't have permission to view this page.", pageTitle: "403", authorised: req.session.authorised, permissions: req.session.permissions, duckFact: duckFact() });
 	}
 });
 
