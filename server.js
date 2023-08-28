3eb6e343e92d97cde1e43f84cb7fdb1baf2d259e
SkySourced <58579786+SkySourced@users.noreply.github.com>
2023-08-28 16:44:56 +1200

    fix(csrf): changed settings

--- server.js
+++ server.js
@@ -40,15 +40,7 @@ app.use(session({ // Set up session
 	secure: (process.env.TARGET === "production")
 }));
 
-app.use(lusca({
-	csrf: true,
-	xframe: "SAMEORIGIN",
-	p3p: "ABCDEF",
-	hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
-	xssProtection: true,
-	nosniff: true,
-	referrerPolicy: "same-origin"
-}));
+app.use(lusca.csrf({angular: true})); // Set up CSRF protection
 
 app.use("/", indexRouter);
 app.use("/users", userRouter); // Use user router
