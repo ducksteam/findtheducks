103e7622fd692111863a12da08623bbcd7d33e30
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2024-09-01 14:39:54 +1200

    style(image): Added sample duck

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 import postgres from "postgres";
-const sql = postgres(process.env.POSTGRES_CONNECTION_STRING + "?sslmode=require");
+const sql = null;
+//const sql = postgres(process.env.POSTGRES_CONNECTION_STRING + "?sslmode=require");
 export default sql; 
