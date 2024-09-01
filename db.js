c5fe43f14d56105f04bc93ca78918828ce01d72c
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2024-09-01 15:31:51 +1200

    fix(db): undeleted db connection

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
 import postgres from "postgres";
-const sql = null;
-//const sql = postgres(process.env.POSTGRES_CONNECTION_STRING + "?sslmode=require");
+const sql = postgres(process.env.POSTGRES_CONNECTION_STRING + "?sslmode=require");
 export default sql; 
